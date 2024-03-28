ac60f142b007cb25d1a596124218470f2bbe2eac
juarez3 <164895654+juarez3@users.noreply.github.com>
2024-03-28 19:25:34 -0300

    fuga

---
+++ c/1/1016.c
@@ -0,0 +1,16 @@
+/*
+fugindo do racha
+*/
+
+#include <stdio.h>
+
+int main() 
+{
+  int distance, minutos
+  scanf("%d", &distance); 
+  minutos = distance * 2; //equação
+
+  printf("%d minutos\n", minutos);
+
+  return 0;
+}
