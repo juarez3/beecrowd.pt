ef1e4fe6cf867a5f773f13143a3bbbf3eb8846b6
juarez3 <164895654+juarez3@users.noreply.github.com>
2024-04-04 18:47:23 -0300

    como acha o valor do meio?

---
+++ c/1/1042.c
@@ -0,0 +1,47 @@
+/*
+/*
+mostra os valores em ordem crescente 
+e
+a entrada
+*/
+#include <stdio.h>
+int main() 
+{
+    int n1, n2, n3;
+    scanf("%d %d %d", &n1, &n2, &n3);
+   {//menor
+    if (n1<n2 && n1<n3) //n1
+        printf("%d\n", n1);
+        else if (n2<n3) //n2
+            printf("%d\n", n2);
+        else //n3
+            printf("%d\n", n3);
+   }
+   {//o do meio
+/* es parte NÃO FUNCIONA*/
+   if (n1<n2 && n2<n3)
+   (n3<n2 && n2<n1)
+        printf("%d\n", n2);
+        else if (n1<n3 && n3<n2)
+    (n2<n3 && n3<n1)
+            printf("%d\n", n3);
+        else 
+            printf("%d\n", n1);
+   }
+   {//maior
+    if (n1>n2 && n1>n3) //n1
+        printf("%d\n", n1);
+        else if (n2>n3) //n2
+            printf("%d\n", n2);
+        else //n3
+            printf("%d\n", n3);
+   } 
+   printf("\n");
+   printf("%d\n",n1);
+   printf("%d\n",n2);
+   printf("%d\n",n3);
+   
+    return 0;
+
+}
+*/
